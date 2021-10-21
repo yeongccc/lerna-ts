2750ca25812bf4e365c50ca0045abec921479450
yangyun <>
2021-10-21 14:13:44 +0800

    feat：添加一个ts实力

--- packages/math-demo/src/demo.ts
+++ packages/math-demo/src/demo.ts
@@ -1,3 +1,20 @@
-export default class Math{
-  
+class Student {
+    fullName: string;
+    constructor(public firstName, public middleInitial, public lastName) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
 }
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person : Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Jane", "M.", "User");
+
+export default greeter(user)
+
